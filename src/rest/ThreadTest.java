da4d96f8ef637ece58abd6246ac879903758fb96
Stefan1de1V <s.o.devringer@student.tudelft.nl>
2015-01-14 09:32:39 +0100

    Busy at the moment, don"t freak pls

--- src/rest/ThreadTest.java
+++ src/rest/ThreadTest.java
@@ -11,11 +11,45 @@ public class ThreadTest implements Runnable {
 	String beginText = "Here is the progress of your match:\n\n";
 	workSpace.setText(beginText);
 
-	String advancedText = "You lost";
-	beginText = beginText + advancedText;
-	workSpace.setText(beginText);
+	//String advancedText = "You lost";
+	//beginText = beginText + advancedText;
+	//workSpace.setText(beginText);
 
+        String methodeOutput = "";
+        
 	System.out.println("Thread has set 'ongoingMatchText'");
+        
+        //Twee teams
+        
+        //Echte stuff
+        MatchLogic thisMatch = new MatchLogic(15,states.StateManager.getLeague().getTeams().get(0),states.StateManager.getLeague().getTeams().get(1));
+        
+        for(int n=0;n<15;n++){
+            Update tickHome = thisMatch.tickHome();
+            Update tickAway = thisMatch.tickAway();
+            switch(tickHome.getType()){
+                case 0: break;
+                case 1: methodeOutput = methodeOutput + "Gele kaart voor " + tickHome.getSpeler() + "!/n"; break;
+                case 2: methodeOutput = methodeOutput + "Rode kaart voor " + tickHome.getSpeler() + "!/n"; break;
+                case 3: methodeOutput = methodeOutput + tickHome.getSpeler() + "is geblesseerd geraakt!/n"; break;
+                case 4: methodeOutput = methodeOutput + "GOAL! Doelpuntenmaker: " +tickHome.getSpeler() + "!/n"; break;
+            }
+            switch(tickAway.getType()){
+                case 0: break;
+                case 1: methodeOutput = methodeOutput + "Gele kaart voor " + tickAway.getSpeler() + "!/n"; break;
+                case 2: methodeOutput = methodeOutput + "Rode kaart voor " + tickAway.getSpeler() + "!/n"; break;
+                case 3: methodeOutput = methodeOutput + tickAway.getSpeler() + "is geblesseerd geraakt!/n"; break;
+                case 4: methodeOutput = methodeOutput + "GOAL! Doelpuntenmaker: " +tickAway.getSpeler() + "!/n"; break;
+            }
+            String display = beginText + methodeOutput;
+            workSpace.setText(display);
+            try{
+                Thread.sleep(1500);
+            }
+            catch(InterruptedException e){
+                System.out.println(e);
+            }
+            }
     }
 
     public static JTextArea getWorkSpace() {
